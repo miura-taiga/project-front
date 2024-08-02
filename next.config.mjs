61752450a6b7c510d63f125e8e461ad9d694237d
三浦大河 <taigav8@gmail.com>
2024-08-03 08:55:28 +0900

    ルートから /top への永続的リダイレクトを追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/top',
+          permanent: true, // 永続的なリダイレクト
+        },
+      ];
+    },
+  };
 
 export default nextConfig;
